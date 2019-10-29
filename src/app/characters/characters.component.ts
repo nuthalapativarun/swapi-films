311493b3a9a1e314a09d608b8c53f6142857508f
Varun Nuthalapati <varun.nuthalapati@trinet.com>
2019-10-28 17:05:40 -0700

    Characters componenet code clean up

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,12 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { SpeciesDetailsService } from './../services/species-details.service';
-import { CharacterDetailsService } from './../services/character-details.service';
-import { FilmDetailsService } from './../services/film-details.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Observable, from, forkJoin} from 'rxjs';
-import { concatAll, switchMap, mergeMap } from 'rxjs/operators';
-
 import { Store } from '@ngrx/store';
 import { AppState } from './../app.state';
 import * as MoviesSelectors from './../shared/store/movies/selectors';
@@ -23,19 +16,15 @@ export class CharactersComponent implements OnInit {
   characters: [] = [];
   filmsData: any;
   currentCharacterData:any = [];
-  currentReduxCharacterData:any = [];
   currentPeopleIds: any[] = [];
   uniqueSpecies = new Set();
-  currentuniqueSpecies = new Set();
   allSpeciesData:{} = {};
-  reduxAllSpeciesData:{} = {};
   data: any;
   movieName: string;
   allCharactersData: any;
   allPeopleIds: any[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, private characterDetailsService: CharacterDetailsService, 
-    private speciesDetailsService: SpeciesDetailsService, private store: Store<AppState>) { }
+  constructor(private router: Router, private route: ActivatedRoute, private store: Store<AppState>) { }
 
   ngOnInit() {
     this.store.select(MoviesSelectors.getFilms).subscribe(data => {
@@ -48,19 +37,17 @@ export class CharactersComponent implements OnInit {
 
     this.store.select(MoviesSelectors.getSpecies).subscribe(data => {
       if (data) {
-        this.reduxAllSpeciesData = data;
+        this.allSpeciesData = data;
         this.speciesDataCheck();
       }
     });
     
-
     this.store.select(MoviesSelectors.getPeople).subscribe(data => {
       if(data){
         this.allCharactersData = data;
         this.allPeopleIds = Object.keys(data);
         this.constructData();
       }
-      
     });
 
     this.route.params.subscribe(
@@ -71,7 +58,6 @@ export class CharactersComponent implements OnInit {
         }else{
           this.characters = this.filmsData[this.id]['characters'];
           this.movieName = this.filmsData[this.id]['title'];
-          // this.construct(); //http
           this.fetchCurrentCharacterData();
         }
       }); 
@@ -81,7 +67,7 @@ export class CharactersComponent implements OnInit {
     if(!this.characters.length){
       return;
     }
-    this.currentPeopleIds = this.characters.map(url => this.deconstructUrl(url));
+    this.currentPeopleIds = this.characters.map(url => this.peopleDeconstructUrl(url));
     let newPeopleIds = [];
     if(this.allPeopleIds && this.allPeopleIds.length > 0){ 
       newPeopleIds = this.currentPeopleIds.filter(id => !this.allPeopleIds.includes(id));
@@ -101,28 +87,28 @@ export class CharactersComponent implements OnInit {
     }
     let peopleIds = this.currentPeopleIds.filter(id => this.allPeopleIds.includes(id));
     if(peopleIds.length === this.currentPeopleIds.length){
-      this.currentReduxCharacterData = this.currentPeopleIds.map(id => this.allCharactersData[id]);
+      this.currentCharacterData = this.currentPeopleIds.map(id => this.allCharactersData[id]);
       this.constructUniqueSpeciesData();
     }
   }
 
   constructUniqueSpeciesData(){
-    this.currentuniqueSpecies = new Set();
-    this.currentReduxCharacterData.forEach((item, index) => {
+    this.uniqueSpecies = new Set();
+    this.currentCharacterData.forEach((item, index) => {
       item['species'].forEach(url => {
         let speciesId = this.speciesDeconstructUrl(url);
-        if(!this.reduxAllSpeciesData[speciesId]){
-          this.currentuniqueSpecies.add(speciesId);
+        if(!this.allSpeciesData[speciesId]){
+          this.uniqueSpecies.add(speciesId);
         }
       });
-      if(this.currentReduxCharacterData.length-1 === index){
+      if(this.currentCharacterData.length-1 === index){
         this.getAbsentSpeciesData();
       }
     })
   }
 
   getAbsentSpeciesData(){
-    let species_ids = Array.from(this.currentuniqueSpecies);
+    let species_ids = Array.from(this.uniqueSpecies);
     if(species_ids.length > 0){
       this.store.dispatch(new MoviesActions.LoadSpecies({'species_id': species_ids}));
     }else{
@@ -131,61 +117,17 @@ export class CharactersComponent implements OnInit {
   }
 
   speciesDataCheck(){
-    for(let key of Array.from(this.currentuniqueSpecies)){
-      if(!this.reduxAllSpeciesData[+key]){
+    for(let key of Array.from(this.uniqueSpecies)){
+      if(!this.allSpeciesData[+key]){
         this.getAbsentSpeciesData();
       }
     }
-    if(this.currentReduxCharacterData.length > 0){
-      this.ccd();
+    if(this.currentCharacterData.length > 0){
+      this.constructCarouselData();
     }
   }
-  ccd(){
-    this.data = this.currentReduxCharacterData.map(item => {
-      let speciesNames = item.species.map(url => {
-        let specieId = this.speciesDeconstructUrl(url);
-        return this.reduxAllSpeciesData[specieId]['name'];
-      });
-      
-      return {
-        title: item.name,
-        secondaryData: speciesNames
-        };
-    });
-  }
-
-  deconstructUrl(url){
-    let startIndex = url.indexOf('/people/');
-    return url.slice(startIndex+8, url.length-1);
-  }
-
-  speciesDeconstructUrl(url){
-    let startIndex = url.indexOf('/species/');
-    return url.slice(startIndex+9, url.length-1);
-  }
-
-  construct(){//Http
-    let peoples_id = this.characters.map(url => this.deconstructUrl(url));
-    let characterSubscription = this.getCharacters(peoples_id);
-    characterSubscription.subscribe(data => {
-      this.currentCharacterData.push(data);
-
-      if(data && data['species']){
-        data['species'].forEach(url => {
-          let speciesId = this.speciesDeconstructUrl(url);
-          if(!this.allSpeciesData[speciesId]){
-            this.uniqueSpecies.add(speciesId);
-          }
-        })
-      }
-      if(this.currentCharacterData.length === peoples_id.length){
-        this.getSpeciesData();
-      }
-    });
-  }
-
-  construcCarouselData(){//Http
-    let data = this.currentCharacterData.map(item => {
+  constructCarouselData(){
+    this.data = this.currentCharacterData.map(item => {
       let speciesNames = item.species.map(url => {
         let specieId = this.speciesDeconstructUrl(url);
         return this.allSpeciesData[specieId]['name'];
@@ -197,34 +139,15 @@ export class CharactersComponent implements OnInit {
         };
     });
   }
-  getSpeciesData(){//Http
-    let speciesIds = Array.from(this.uniqueSpecies);
-    let speciesData = [];
-    let speciesSubscription = this.getItems(speciesIds); 
-    speciesSubscription.subscribe(data => {
-      speciesData.push(data);
-      if(speciesData.length === speciesIds.length){
-        speciesIds.forEach((ids, index) => {
-          this.allSpeciesData[+ids] = speciesData[index];
-          if(index === speciesIds.length-1){
-            this.construcCarouselData();
-          }
-        });
-      }
-    });
+
+  peopleDeconstructUrl(url){
+    let startIndex = url.indexOf('/people/');
+    return url.slice(startIndex+8, url.length-1);
   }
 
-  getItems(ids): Observable<any> {//Http
-    return <Observable<any>> forkJoin(
-      ids.map(id => <Observable<any>> this.speciesDetailsService.getSpecies(id))
-    ).pipe(concatAll());
-  }
-
-
-  getCharacters(ids): Observable<any> {//Http
-    return <Observable<any>> forkJoin(
-      ids.map(id => <Observable<any>> this.characterDetailsService.getCharacter(id))
-    ).pipe(concatAll());
+  speciesDeconstructUrl(url){
+    let startIndex = url.indexOf('/species/');
+    return url.slice(startIndex+9, url.length-1);
   }
 
   back(){
--- src/app/shared/store/movies/effects.ts
+++ src/app/shared/store/movies/effects.ts
@@ -2,22 +2,16 @@ import { SpeciesDetailsService } from './../../../services/species-details.servi
 import { CharacterDetailsService } from './../../../services/character-details.service';
 import { FilmDetailsService } from './../../../services/film-details.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 import * as  MoviesActions from './actions';
 import { MoviesActionTypes } from './types';
-// import { Company, Species } from './models';
-import { AppState } from '../../../app.state';
-
-import { Store } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Observable, of, forkJoin } from 'rxjs';
-import { concatAll, catchError, map, switchMap, tap, takeLast, startWith, exhaustMap, withLatestFrom } from 'rxjs/operators';
+import { Observable, forkJoin } from 'rxjs';
+import { map, exhaustMap } from 'rxjs/operators';
 
 
 @Injectable()
 export class MoviesEffects {
-    constructor(private actions: Actions, private store: Store<AppState>, private filmDetailsService: FilmDetailsService,
+    constructor(private actions: Actions, private filmDetailsService: FilmDetailsService,
                 private characterDetailsService: CharacterDetailsService, private speciesDetailsService: SpeciesDetailsService){}
 
 
@@ -44,7 +38,6 @@ export class MoviesEffects {
         ofType<MoviesActions.LoadPeople>(MoviesActionTypes.LoadPeople),
         map(action => action.payload),
         exhaustMap((payload) => {
-
             return <Observable<any>> forkJoin(
                 payload.people_id.map(ids => <Observable<any>> this.characterDetailsService.getCharacter(ids))
               ).pipe(map(successData => {
@@ -65,11 +58,10 @@ export class MoviesEffects {
     loadSpecies = this.actions.pipe(
         ofType<MoviesActions.LoadSpecies>(MoviesActionTypes.LoadSpecies),
         map(action => action.payload),
-        exhaustMap((payload) => {//debugger
-
+        exhaustMap((payload) => {
             return <Observable<any>> forkJoin(
                 payload.species_id.map(ids => <Observable<any>> this.speciesDetailsService.getSpecies(ids))
-              ).pipe(map(successData => {//debugger
+              ).pipe(map(successData => {
                   let ids = payload.species_id;
                   let data = {}
                   successData.forEach((item, index) => {
